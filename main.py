0202fa3c092561907eea08c0e55bcde2dbf6541e
xsren <bestrenxs@gmail.com>
2017-06-27 14:47:04 +0800

    update code

--- main.py
+++ main.py
@@ -11,14 +11,11 @@
 
 """
 
-from lib import
+from libs.wx_bot import WxBot
 
 def run():
-    app = Application()
-    # 设置窗口标题:
-    app.master.title('Hello World')
-    # 主消息循环:
-    app.mainloop()
+    wb = WxBot()
+    wb.run()
 
 
 if __name__ == '__main__':
